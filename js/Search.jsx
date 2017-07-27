ebded224ad0d42577d33153cd1b9af71fe1251a6
Kevin Nguyen <knguye01@pros.com>
2017-07-27 11:50:25 -0500

    Implement filter and search for Search.jsx component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 
-const Search = () => (
-  <div className="search">
-    {preload.shows.map((show, i) =>
-      <ShowCard show={show} key={i} />
-    )}
-  </div>
-)
+class Search extends Component { 
+  state = {
+    searchTerm: ''
+  };
+  
+  handleSearchTermChange = (event) => {
+    this.setState({searchTerm: event.target.value})
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input 
+            onChange={this.handleSearchTermChange} 
+            value={this.state.searchTerm} 
+            type="text" 
+            placeholder="Search" 
+          />
+        </header>
+        <div>
+          {preload.shows
+          .filter(show => 
+            `${show.title} ${show.description}`.toUpperCase()
+              .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+          )
+          .map(show =>
+            <ShowCard show={show} key={show.imdbID} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
